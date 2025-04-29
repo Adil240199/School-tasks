d8077b17bf0178c145e8ffecbf2a8e65832397ce
Adilzhan Mukhametov <106250697+Adil240199@users.noreply.github.com>
2025-04-29 10:08:19 +0200

    Update menu.js

--- coffee-house/menu.js
+++ coffee-house/menu.js
@@ -1,21 +1,25 @@
-const burgerButtonMain = document.querySelector(".burgerButtonMain"),
-  burgerButton = document.querySelector(".burgerButton"),
-  adaptivMenu = document.querySelector(".adaptivMenu"),
-  linkHeaders = document.querySelectorAll(".linkHeader"),
-  navHeader = document.querySelector(".navHeader");
+const burgerButtonMain = document.querySelector(".burgerButtonMain");
+const burgerButton = document.querySelector(".burgerButton");
+const adaptivMenu = document.querySelector(".adaptivMenu");
+const linkHeaders = document.querySelectorAll(".linkHeader");
+const navHeader = document.querySelector(".navHeader");
+
 burgerButtonMain.addEventListener("click", () => {
   const isMenuOpen = adaptivMenu.classList.contains("menuAdaptivActive");
+
   if (!isMenuOpen) {
     adaptivMenu.style.transform = "translateX(0%)";
     burgerButton.classList.add("burgerButtonActive");
-   } else {
+  } else {
     adaptivMenu.style.transform = "translateX(100%)";
     burgerButton.classList.remove("burgerButtonActive");
     burgerButton.classList.add("burgerButton");
   }
+
   adaptivMenu.classList.toggle("menuAdaptivActive");
   navHeader.classList.toggle("navHeaderActive");
 });
+
 linkHeaders.forEach((item) => {
   item.addEventListener("click", () => {
     adaptivMenu.style.transform = "translateX(100%)";
@@ -26,49 +30,56 @@ linkHeaders.forEach((item) => {
   });
 });
 
-
-
 import products from "./products.json" assert { type: "json" };
+
 const kinds = document.querySelectorAll(".kind");
+const buttonMore = document.querySelector(".refresh");
+
 function generateFile(category = "coffee") {
   const categories = document.querySelector(".catalog");
   let localIndex = 0;
   categories.innerHTML = "";
+
   products.forEach((item) => {
-    if (item.category == category) {
+    if (item.category === category) {
       const coffeeElement = document.createElement("div");
       coffeeElement.classList.add("kindOfCoffee");
-      if (localIndex > 3){
-        coffeeElement.classList.add("kindOfCoffeeHide")
+      if (localIndex > 3) {
+        coffeeElement.classList.add("kindOfCoffeeHide");
       }
       coffeeElement.innerHTML = `
-              <img class="imgCatalog" src="${item.imageSrc}" alt="coffeeImage">
-              <h5>${item.name}</h5>
-              <p class="describeCoffee">${item.description}</p>
-              <p class="price">$${item.price}</p>
-          `;
-          localIndex++;
+        <img class="imgCatalog" src="${item.imageSrc}" alt="coffeeImage">
+        <h5>${item.name}</h5>
+        <p class="describeCoffee">${item.description}</p>
+        <p class="price">$${item.price}</p>
+      `;
+      localIndex++;
       categories.appendChild(coffeeElement);
     }
   });
 }
-kinds[0].addEventListener("click", function () {
+
+kinds[0].addEventListener("click", () => {
   generateFile("coffee");
   buttonMore.style.display = "flex";
 });
-kinds[1].addEventListener("click", function () {
+
+kinds[1].addEventListener("click", () => {
   generateFile("tea");
 });
-kinds[2].addEventListener("click", function () {
+
+kinds[2].addEventListener("click", () => {
   generateFile("dessert");
   buttonMore.style.display = "flex";
-
 });
+
 let currentActiveElement = null;
+
 if (kinds.length > 0) {
   currentActiveElement = kinds[0];
   currentActiveElement.classList.add("Kindactive");
 }
+
 kinds.forEach((item) => {
   item.addEventListener("click", () => {
     if (currentActiveElement) {
@@ -79,8 +90,6 @@ kinds.forEach((item) => {
   });
 });
 
-
-const buttonMore = document.querySelector(".refresh");
 buttonMore.addEventListener("click", (event) => {
   event.preventDefault();
   buttonMore.style.display = "none";
